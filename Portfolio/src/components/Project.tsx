e24e1fd18cf43cd186db6190fb7be96babc54143
dsabelli <dsabelli3@gmail.com>
2022-11-26 09:41:58 -0500

    add icons for links

--- Portfolio/src/components/Project.tsx
+++ Portfolio/src/components/Project.tsx
@@ -1,4 +1,6 @@
 import React, { useId } from "react";
+import ExternalLink from "../../public/ExternalLink";
+import Github from "../../public/Github";
 
 type Position = "left" | "right";
 
@@ -22,29 +24,33 @@ const Project = ({
   position,
 }: Props) => {
   const id = useId();
-  const techEls = tech.map((t, idx) => <li key={`${id}-${idx}`}>{t}</li>);
+  const techEls = tech.map((t, idx) => <li key={`${id}-${idx}`}>-{t}</li>);
 
   return (
     <div className="p-4">
       <div className="hero shadow-2xl rounded-lg bg-base-200">
         <div
-          className={`hero-content flex-col ${
+          className={`hero-content flex-col h-full ${
             position === "left" ? "md:flex-row-reverse" : "md:flex-row"
           }`}
         >
-          <div className="md:w-1/2">
-            <h4>{title}</h4>
+          <div className="md:w-1/2 h-full">
+            <h4 className="text-xl font-bold justify-self-start">{title}</h4>
             <p className="py-6">{description}</p>
-            <ul className="flex">{techEls}</ul>
-            <div className="flex flex-end">
-              <a href={webLink}>webLink</a>
-              <a href={githubLink}>githubLink</a>
+            <ul className="flex">Stack:{techEls}</ul>
+            <div className="flex flex-end gap-2">
+              <a href={webLink}>
+                <ExternalLink />
+              </a>
+              <a href={githubLink}>
+                <Github />
+              </a>
             </div>
           </div>
-          <figure className="md:w-1/2">
+          <figure className="md:w-1/2 ">
             <img
               src={image}
-              className="max-w-sm rounded-lg shadow-2xl w-full"
+              className="max-w-sm max-h-96 rounded-lg shadow-2xl w-full"
             />
           </figure>
         </div>
