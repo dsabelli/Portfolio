7d2410b9e7f172407575e1c69f881f49f63c039e
dsabelli <dsabelli3@gmail.com>
2022-12-01 13:54:43 -0500

    configure theme

--- Portfolio/tailwind.config.cjs
+++ Portfolio/tailwind.config.cjs
@@ -1,6 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#e8c988",
+
+          secondary: "#ffadc1",
+
+          accent: "#b5e57e",
+
+          neutral: "#312735",
+
+          "base-100": "#2F3950",
+
+          info: "#7EBCDD",
+
+          success: "#53EAD6",
+
+          warning: "#F48B0B",
+
+          error: "#F35356",
+        },
+      },
+    ],
+  },
   theme: {
     extend: {},
   },
