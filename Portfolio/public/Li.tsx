c86e10f907c89a9b290bd00deea885204c39db24
dsabelli <dsabelli3@gmail.com>
2022-12-02 10:55:00 -0500

    create custom list component

---
+++ Portfolio/public/Li.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Li = ({ children }: { children: string }) => {
+  return (
+    <li className="flex items-center gap-2">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="feather feather-code text-primary"
+      >
+        <polyline points="16 18 22 12 16 6"></polyline>
+        <polyline points="8 6 2 12 8 18"></polyline>
+      </svg>
+      {children}
+    </li>
+  );
+};
+
+export default Li;
